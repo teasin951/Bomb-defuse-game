bb4644b1175df540c6ed63be3656ec614b637bff
Tomáš Saatzer <t.saatzer@seznam.cz>
2023-07-01 10:15:28 +0200

    Improve animation memory efficiency

--- include/components/matrix/animations.h
+++ include/components/matrix/animations.h
@@ -8,12 +8,14 @@
 
 /* defining frames here to avoid dynamic allocation */
 #define DANCING_EXCLE_FRAMES 2
+#define DANCING_CROSS_FRAMES 6
 
 /**
  * @class Base class for animations
  * 
  * This class holds basic template for animations.
- * Animations are done by filling 'frames' with arrays of pixel values.
+ * Animations are done by filling the base frame and then assigning actions 
+ * to frame changes. Animation is done by changing the base frame or by replacing it.
 */
 class Animation {
 public:
@@ -31,16 +33,19 @@ public:
      * Most importantly, it sets the 'start_millis' variable to the current time and put the starting image to leds.
      * 
      * @param leds array of CRGBs used by FastLED to display
+     * @return false as we shall not move frames after this function call
     */
-    virtual void resetAnimation( CRGB (&leds)[NUM_LEDS] ) {
+    virtual bool resetAnimation( CRGB (&leds)[NUM_LEDS] ) {
         for (uint8_t y = 0; y < NUM_Y; y++) {
             for (uint8_t x = 0; x < NUM_X; x++) {
-                leds[(y*NUM_X)+x] = base_frame[ (y*NUM_X)+x ];
+                leds[(y*NUM_X)+x] = CRGB(0,0,0);
             }
         }
 
         frame = 0;
         start_millis = millis();
+
+        return false;
     }
 
     /**
@@ -79,24 +84,79 @@ public:
         }
     }
 
+    /**
+     * @brief Move entire displayed figure right
+     * 
+     * @param leds array of CRGBs used by FastLED to display
+    */
+    bool moveRight( CRGB (&leds)[NUM_LEDS] ) {
+        for (uint8_t y = 0; y < NUM_Y; y++) {
+            for (uint8_t x = NUM_X - 1; x > 0; x--) {
+                leds[(y*NUM_X)+x] = leds[ (y*NUM_X) + x - 1 ];
+            }
+        }
+        for (uint8_t y = 0; y < NUM_Y; y++) {
+            leds[(y*NUM_X) + 0] = CRGB(0,0,0);
+        }
+        return true;
+    }
+
+    /**
+     * @brief Move entire displayed figure left
+     * 
+     * @param leds array of CRGBs used by FastLED to display
+    */
+    bool moveLeft( CRGB (&leds)[NUM_LEDS] ) {
+        for (uint8_t y = 0; y < NUM_Y; y++) {
+            for (uint8_t x = 0; x < NUM_X - 1; x++) {
+                leds[(y*NUM_X)+x] = leds[ (y*NUM_X) + x + 1 ];
+            }
+        }
+        for (uint8_t y = 0; y < NUM_Y; y++) {
+            leds[(y*NUM_X) + NUM_X - 1] = CRGB(0,0,0);
+        }
+        return true;
+    }
+
+    /**
+     * @brief Move entire displayed figure up
+     * 
+     * @param leds array of CRGBs used by FastLED to display
+    */
+    bool moveUp( CRGB (&leds)[NUM_LEDS] ) {
+        for (uint8_t y = 0; y < NUM_Y - 1; y++) {
+            for (uint8_t x = 0; x < NUM_X; x++) {
+                leds[(y*NUM_X)+x] = leds[ (y*NUM_X + NUM_X) + x ];
+            }
+        }
+        for (uint8_t x = 0; x < NUM_X; x++) {
+            leds[(NUM_Y - 1)*NUM_Y + x] = CRGB(0,0,0);
+        }
+        return true;
+    }
+
+    /**
+     * @brief Move entire displayed figure down
+     * 
+     * @param leds array of CRGBs used by FastLED to display
+    */
+    bool moveDown( CRGB (&leds)[NUM_LEDS] ) {
+        for (uint8_t y = NUM_Y - 1; y > 0; y--) {
+            for (uint8_t x = 0; x < NUM_X; x++) {
+                leds[(y*NUM_X)+x] = leds[ (y*NUM_X - NUM_X) + x ];
+            }
+        }
+        for (uint8_t x = 0; x < NUM_X; x++) {
+            leds[x] = CRGB(0,0,0);
+        }
+        return true;
+    }
+
 protected:
     uint32_t start_millis = 0;  /**< Millis when we switched to this animation */
     uint16_t frame = 0;  /**< Animation frame currently displayed */
     uint16_t frame_count = 1;  /**< Number of frames in the animation, change in constructor!!! */
     uint16_t next_frame_in = 40000;  /**< ms between frames, change in constructor!!! */
-
-    const CRGB base_frame[NUM_LEDS] = {
-        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
-        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
-        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
-        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
-        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
-        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
-        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
-        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
-    };  /**< The first base frame for the animation */
-
-    void (*actions[1])( CRGB (&)[NUM_LEDS] ) = { nullptr };  /**< Actions to perform when changing to the next frame */
 };
 
 
@@ -108,7 +168,7 @@ public:
     }
     virtual ~DancingExcle() = default;
 
-    void resetAnimation( CRGB (&leds)[NUM_LEDS] ) override {
+    bool resetAnimation( CRGB (&leds)[NUM_LEDS] ) override {
         for (uint8_t y = 0; y < NUM_Y; y++) {
             for (uint8_t x = 0; x < NUM_X; x++) {
                 leds[(y*NUM_X)+x] = base_frame[ (y*NUM_X)+x ];
@@ -117,17 +177,8 @@ public:
 
         frame = 0;
         start_millis = millis();
-    }
 
-    void moveRight( CRGB (&leds)[NUM_LEDS] ) {
-        for (uint8_t y = 0; y < NUM_Y; y++) {
-            for (uint8_t x = NUM_X; x > 0; x--) {
-                leds[(y*NUM_X)+x] = leds[ (y*NUM_X) + x - 1 ];
-            }
-        }
-        for (uint8_t y = 0; y < NUM_Y; y++) {
-            leds[(y*NUM_X) + 0] = CRGB(0,0,0);
-        }
+        return false;
     }
 
     void proceed( CRGB (&leds)[NUM_LEDS] ) override {
@@ -135,16 +186,8 @@ public:
         // time to change frames
         if( millis() > start_millis + (frame+1) * next_frame_in ) {
 
-            // do asociated action
-            (this->*actions[frame])(leds);
-
-            // frame overflow
-            if( frame >= frame_count - 1 ) {
-                resetAnimation(leds);
-            }
-
-            // frame advancement
-            else {
+            // do asociated action and decide if we need frame advancement
+            if( (this->*actions[frame])(leds) ) {
                 frame++;
             }
         }
@@ -162,16 +205,82 @@ private:
         CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
     };  /**< The first base frame for the animation */
 
-    void (DancingExcle::*actions[DANCING_EXCLE_FRAMES])( CRGB (&)[NUM_LEDS] ) = { 
-        &DancingExcle::moveRight, nullptr };  /**< Actions to perform when changing to the next frame */
+    bool (DancingExcle::*actions[DANCING_EXCLE_FRAMES])( CRGB (&)[NUM_LEDS] ) = { 
+        &DancingExcle::moveRight, &DancingExcle::resetAnimation };  /**< Actions to perform when changing to the next frame */
 };
 
 
 class DancingCross : public Animation {
 public:
-    DancingCross();
+    DancingCross() {
+        frame_count = DANCING_CROSS_FRAMES;
+        next_frame_in = 200;
+    }
     virtual ~DancingCross() = default;
 
+    void proceed( CRGB (&leds)[NUM_LEDS] ) override {
+
+        // time to change frames
+        if( millis() > start_millis + (frame+1) * next_frame_in ) {
+
+            // do asociated action and decide if we need frame advancement
+            if( (this->*actions[frame])(leds) ) {
+                frame++;
+            }
+        }
+    }
+    bool resetAnimation( CRGB (&leds)[NUM_LEDS] ) override {
+        for (uint8_t y = 0; y < NUM_Y; y++) {
+            for (uint8_t x = 0; x < NUM_X; x++) {
+                leds[(y*NUM_X)+x] = base_frame[ (y*NUM_X)+x ];
+            }
+        }
+
+        frame = 0;
+        start_millis = millis();
+
+        return false;
+    }
+
+    bool leftDown( CRGB (&leds)[NUM_LEDS] ) {
+        moveDown( leds );
+        moveLeft( leds );
+        return true;
+    }
+
+    bool leftUp( CRGB (&leds)[NUM_LEDS] ) {
+        moveUp( leds );
+        moveLeft( leds );
+        return true;
+    }
+
+    bool rightUp( CRGB (&leds)[NUM_LEDS] ) {
+        moveUp( leds );
+        moveRight( leds );
+        return true;
+    }
+
+    bool rightDown ( CRGB (&leds)[NUM_LEDS] ) {
+        moveDown( leds );
+        moveRight( leds );
+        return false;
+    }
+
 private:
+    const CRGB base_frame[NUM_LEDS] = {
+        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
+        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(192,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(192,0,0),
+        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(192,0,0),CRGB(0,0,0),CRGB(192,0,0),CRGB(0,0,0),
+        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(192,0,0),CRGB(0,0,0),CRGB(0,0,0),
+        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(192,0,0),CRGB(0,0,0),CRGB(192,0,0),CRGB(0,0,0),
+        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(192,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(192,0,0),
+        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
+        CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),CRGB(0,0,0),
+    };  /**< The first base frame for the animation */
+
+    bool (DancingCross::*actions[DANCING_CROSS_FRAMES])( CRGB (&)[NUM_LEDS] ) = { 
+        &DancingCross::leftDown, &DancingCross::leftDown,
+        &DancingCross::leftUp, &DancingCross::rightUp, 
+        &DancingCross::rightUp, &DancingCross::resetAnimation };  /**< Actions to perform when changing to the next frame */
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@
 
 Joystick joystick;  /**< Create joystick struct */
 
-// DancingCross c;
+DancingCross c;
 DancingExcle d;
-// Animation b;
+Animation b;
 
 int j = 0;
 
@@ -21,41 +21,41 @@ void setup() {
   pinSetup();
   setupFastLED();
 
-  d.setAnimation(leds);
 }
 
 void loop() {
   readADKeyboard();
   readJoystick(joystick, JOYSTICK_SW, JOYSTICK_X, JOYSTICK_Y);
 
-  display(d);
   /* --- Experiment --- */
-  // if( adkeyboard.left == ButtonState::PRESSED ) {
-  //   j = 1;
-  // }
-  // else if( adkeyboard.right == ButtonState::PRESSED ) {
-  //   j = 2;
-  // }
-  // else if( adkeyboard.up == ButtonState::PRESSED ) {
-  //   j = 3;
-  // }
-  // else if( adkeyboard.enter == ButtonState::PRESSED ) {
-  //   j = 0;
-  // }
+  if( adkeyboard.right == ButtonState::PRESSED ) {
+    d.setAnimation(leds);
+    j = 2;
+  }
+  else if( adkeyboard.left == ButtonState::PRESSED ) {
+    j = 1;
+  }
+  else if( adkeyboard.up == ButtonState::PRESSED ) {
+    j = 3;
+  }
+  else if( adkeyboard.enter == ButtonState::PRESSED ) {
+    b.setAnimation(leds);
+    j = 0;
+  }
 
-  // if( j == 1 ) {
-  //   display(c);
-  // }
-  // else if( j == 2 ) {
-  //   display(d);
-  // }
-  // else if( j == 3 ) {
-  //   joystickDisplay( joystick );
-  // }
-  // else {
-  //   display(b);
-  // }
+  if( j == 2 ) {
+    display(d);
+  }
+  else if( j == 1 ) {
+    display(c);
+  }
+  else if( j == 3 ) {
+    joystickDisplay( joystick );
+  }
+  else {
+    display(b);
+  }
   /* --- ~Experiment --- */
 
-  delay(1000);
+  delay(50);
 }
