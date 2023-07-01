cba254eb3ac27f37a127dc95e8a4470ab5b084bf
Tomáš Saatzer <t.saatzer@seznam.cz>
2023-07-01 21:25:01 +0200

    Implement sequence handeling

--- include/components/keypad.h
+++ include/components/keypad.h
@@ -4,6 +4,12 @@
 
 #include "pins.h"
 
+/**
+ * @brief Simplify keypad handeling
+ * 
+ * setupKeypad() has to be called once and keypad.getKey() periodically
+*/
+
 const uint8_t ROWS = 4; //four rows
 const uint8_t COLS = 3; //four columns
 //define the cymbols on the buttons of the keypads
@@ -18,3 +24,129 @@ byte colPins[COLS] = {KEY_LEFT_COL, KEY_MID_COL, KEY_RIGHT_COL}; //connect to th
 
 //initialize an instance of class NewKeypad
 Keypad keypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void nada() {}  // just nothing, placeholder
+
+// Create a struct holding keypad info to verify sequences
+struct KeypadInfo {
+  char sequence[16];  /**< currently typed in sequence */
+  uint8_t length = 0;  /**< current number of typed in characters */
+
+  char match_sequence[16];  /**< sequence to match */
+  uint8_t match_length = 16;  /**< length of the sequence to match */
+
+  void (*pressed)(void) = nada;  /**< pointer to a function called when a button is pressed */
+  void (*matched)(void) = nada;  /**< pointer to a function called when sequences match */
+  void (*noMatch)(void) = nada;  /**< pointer to a function called when sequences do not match */
+  void (*clear)(void) = nada;  /**< pointer to a function called when the sequence is manually cleared */
+  void (*overflow)(void) = nada;  /**< pointer to a function called when the sequence entered is too long */
+};
+
+struct KeypadInfo keypad_info;
+
+
+/**
+ * @brief Verifies that 'sequence' and 'match_sequence' match in 'match_length' number of characters
+ * 
+ * Also calls noMatch() or matched()
+*/
+bool verifyMatch() {
+  for( uint8_t i = 0; i < keypad_info.match_length; i++ ) {
+    if( !( keypad_info.sequence[i] == keypad_info.match_sequence[i] ) ) {
+      keypad_info.noMatch();
+      return false;
+    }
+  }
+
+  keypad_info.matched();
+  return true;
+}
+
+/**
+ * @brief Sets 'sequence' to all spaces and length = 0
+*/
+void clearSequence() {
+  for( uint8_t i = 0; i < 16; i++ ) {
+    keypad_info.sequence[i] = ' ';
+  }
+  keypad_info.length = 0;
+}
+
+
+/**
+ * @brief Event called when button is pressed, adds symbols to 'sequence'
+ * 
+ * @param x KeypadEvent passed from event listener of Keypad.h
+*/
+void eventHandler( KeypadEvent x ) {
+  KeypadInfo & k = keypad_info;
+
+  // Verify that it is a press
+  if( keypad.isPressed(x) ) {
+
+    // Enter pressed
+    if( x == '#' ) {
+      verifyMatch();
+      clearSequence();
+    }
+
+    // Clear pressed 
+    else if( x == '*' ) {
+      clearSequence();
+      k.clear();
+    }
+
+    // Number pressed
+    else {
+      k.pressed();
+
+      // We are not at the end of the sequence yet
+      if( k.length < k.match_length ) {
+        k.sequence[k.length] = x;
+        k.length += 1;
+      }
+
+      // We are out of space
+      else {
+        // Not permisive, the sequence will not be checked when overflowed
+        clearSequence();
+        k.overflow();
+      }
+
+    }
+  }
+}
+
+/**
+ * @brief Set sequence to match and functions to be called when that happends (or does not)
+ * 
+ * // Can be made lenght non-disclosing by setting size = 16 and filling the not used part of the sequence by ' '
+ * 
+ * @param match sequence that shall be matched by the user
+ * @param size length of the sequence
+ * @param pressed function to call when a button is pressed
+ * @param s_matched function to call when the correct sequence is entered
+ * @param s_did_not_match function to call when an incorrect sequence is entered
+ * @param s_cleared function to call when the sequence is manually cleared
+ * @param s_overflowed function to call when the user tries to enter a too long sequence
+*/
+void setSequence( char * match, uint8_t size, 
+    void (*button_pressed)(void), void (*s_matched)(void), void (*s_did_not_match)(void), void (*s_cleared)(void), void (*s_overflowed)(void) ) {
+
+  for( uint8_t i = 0; i < size; i++ ) {
+    keypad_info.match_sequence[i] = match[i];
+  }
+
+  keypad_info.match_length = size;
+  keypad_info.pressed = button_pressed;
+  keypad_info.matched = s_matched;
+  keypad_info.noMatch = s_did_not_match;
+  keypad_info.clear = s_cleared;
+  keypad_info.overflow = s_overflowed;
+}
+
+
+void setupKeypad() {
+  keypad.addEventListener(eventHandler);
+  clearSequence();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,41 @@
 
 Joystick joystick;  /**< Create joystick struct */
 
+/* Experiment */
+char test[2] = { '1', '2' };
+
+void playSound0() {
+  tone(BUZZER_1, 200, 100);
+}
+void playSound1() {
+  tone(BUZZER_1, 400, 100);
+}
+void playSound2() {
+  tone(BUZZER_1, 800, 100);
+}
+void playSound3() {
+  tone(BUZZER_1, 1200, 100);
+}
+void playSound4() {
+  tone(BUZZER_1, 1600, 100);
+}
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
 
   pinSetup();
   setupFastLED();
+  setupKeypad();
   lcd.init();
+
+  setSequence(test, 2, playSound0, playSound1, playSound2, playSound3, playSound4);  // Experiment
 }
 
 void loop() {
   readADKeyboard();
   readJoystick(joystick, JOYSTICK_SW, JOYSTICK_X, JOYSTICK_Y);
-
-  Serial.println(keypad.getKey());
+  keypad.getKey();
 
   delay(50);
 }
