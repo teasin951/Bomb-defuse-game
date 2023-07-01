13feba96c7d5adaea9f53c0143701b8af79aaffc
Tomáš Saatzer <t.saatzer@seznam.cz>
2023-07-01 14:54:56 +0200

    Remove experiment

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,6 @@
 
 Joystick joystick;  /**< Create joystick struct */
 
-DancingCross c;
-DancingExcle d;
-Animation b;
-
 int j = 0;
 
 void setup() {
@@ -26,36 +22,6 @@ void setup() {
 void loop() {
   readADKeyboard();
   readJoystick(joystick, JOYSTICK_SW, JOYSTICK_X, JOYSTICK_Y);
-
-  /* --- Experiment --- */
-  if( adkeyboard.right == ButtonState::PRESSED ) {
-    d.setAnimation(leds);
-    j = 2;
-  }
-  else if( adkeyboard.left == ButtonState::PRESSED ) {
-    j = 1;
-  }
-  else if( adkeyboard.up == ButtonState::PRESSED ) {
-    j = 3;
-  }
-  else if( adkeyboard.enter == ButtonState::PRESSED ) {
-    b.setAnimation(leds);
-    j = 0;
-  }
-
-  if( j == 2 ) {
-    display(d);
-  }
-  else if( j == 1 ) {
-    display(c);
-  }
-  else if( j == 3 ) {
-    joystickDisplay( joystick );
-  }
-  else {
-    display(b);
-  }
-  /* --- ~Experiment --- */
 
   delay(50);
 }
