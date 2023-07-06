a7a0776e0b9648bc789778c4bc65497748b464c6
Tomáš Saatzer <t.saatzer@seznam.cz>
2023-07-06 22:28:52 +0200

    Make ADKeyboard reading more robust

--- include/components/adkeyboard.h
+++ include/components/adkeyboard.h
@@ -29,47 +29,67 @@ struct Keyboard {
 struct Keyboard adkeyboard;  
 
 uint16_t lastDebounceMillis = 0;
-uint16_t debounceDelay = 50;
+const uint16_t debounceDelay = 50;
+
+uint16_t lastSampleMillis = 0;
+const uint16_t sampleDelay = 80;
+
+
+/* 
+ * To prevent ex. left button release
+ * trigger right button due to parasitic capacitance 
+ */
+uint8_t button_pressed = 0;  /**< A button was down last reading 0 - nothing, 1 - left, ...*/
 
   
 void leftButtonClick() {
     adkeyboard.left = PRESSED;
+    button_pressed = 1;
 }  
   
 void leftButtonHold() {
     adkeyboard.left = HOLD;
+    button_pressed = 1;
 }  
   
 void rightButtonClick() {
     adkeyboard.right = PRESSED;
+    button_pressed = 2;
 }  
   
 void rightButtonHold() {
     adkeyboard.right = HOLD;
+    button_pressed = 2;
 }  
   
 void upButtonClick() {
     adkeyboard.up = PRESSED;
+    button_pressed = 3;
 }  
   
 void upButtonHold() {
     adkeyboard.up = HOLD;
+    button_pressed = 3;
 }  
   
 void downButtonClick() {
     adkeyboard.down = PRESSED;
+    button_pressed = 4;
 }  
   
 void downButtonHold() {
     adkeyboard.down = HOLD;
+    button_pressed = 4;
 }  
   
 void enterButtonClick() {
     adkeyboard.enter = PRESSED;
+    button_pressed = 5;
 }  
   
 void enterButtonHold() {
     adkeyboard.enter = HOLD;
+    button_pressed = 5;
 }  
   
 void buttonRelease() {
@@ -78,6 +98,8 @@ void buttonRelease() {
     adkeyboard.up = RELEASED;
     adkeyboard.down = RELEASED;
     adkeyboard.enter = RELEASED;
+
+    button_pressed = 0;
 }     
    
 /**
@@ -85,52 +107,59 @@ void buttonRelease() {
 */
 void readADKeyboard() {
     /* Debounce */
-    if( millis() - lastDebounceMillis > debounceDelay ) {
+    if( millis() - lastDebounceMillis > debounceDelay && millis() - lastSampleMillis > sampleDelay ) {
 
         uint16_t value = analogRead(ADKEYBOARD);
 
-        /* left button */
-        if( value <= 75 ) {
-            if( adkeyboard.left == RELEASED ) {
-                leftButtonClick();
+        /* enter button */
+        if( 635 < value && value <= 900 && ( button_pressed == 0 || button_pressed == 5 ) ) {
+            if( adkeyboard.enter == RELEASED ) {
+                Serial.print("Enter: ");
+                Serial.println(value);
+                enterButtonClick();
                 lastDebounceMillis = millis();
             }
         }
-        
+
         /* right button */
-        else if( 400 < value && value <= 635 ) {
+        else if( 400 < value && value <= 635 && ( button_pressed == 0 || button_pressed == 2 ) ) {
             if( adkeyboard.right == RELEASED ) {
+                Serial.print("Right: ");
+                Serial.println(value);
                 rightButtonClick();
                 lastDebounceMillis = millis();
             }
         }
 
-        /* up button */
-        else if( 75 < value && value <= 230 ) {
-            if( adkeyboard.up == RELEASED ) {
-                upButtonClick();
-                lastDebounceMillis = millis();
-            }
-        }
-
         /* down button */
-        else if( 230 < value && value <= 400 ) {
+        else if( 230 < value && value <= 400 && ( button_pressed == 0 || button_pressed == 4 ) ) {
             if( adkeyboard.down == RELEASED ) {
                 downButtonClick();
                 lastDebounceMillis = millis();
             }
         }
 
-        /* enter button */
-        else if( 635 < value && value <= 900 ) {
-            if( adkeyboard.enter == RELEASED ) {
-                enterButtonClick();
+        /* up button */
+        else if( 75 < value && value <= 230 && ( button_pressed == 0 || button_pressed == 3 ) ) {
+            if( adkeyboard.up == RELEASED ) {
+                upButtonClick();
                 lastDebounceMillis = millis();
             }
         }
 
-        else {
-            buttonRelease();
+        /* left button */
+        else if( value <= 75 && ( button_pressed == 0 || button_pressed == 1 ) ) {
+            if( adkeyboard.left == RELEASED ) {
+                leftButtonClick();
+                lastDebounceMillis = millis();
+            }
         }
+
+        else if( 900 < value && !(button_pressed == 0) ) {
+            buttonRelease();
+            lastDebounceMillis = millis();
+        }
+
+        lastSampleMillis = millis();
     }
 }
