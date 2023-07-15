e31508b1bc43d1279eccdec19e1a2334fd31dd24
Tomáš Saatzer <t.saatzer@seznam.cz>
2023-07-15 22:55:47 +0200

    Add comment

--- include/game/variables.h
+++ include/game/variables.h
@@ -9,7 +9,7 @@
 uint32_t game_start_millis = 0;  /**< When did the game start */
 uint32_t game_countdown_amount = 900000;  /**< How long for a game, default: 15 min*/
 uint32_t game_countdown_penalty = 0;  /**< To make the countdown faster, this will be subtracted from time every second */
-uint8_t mistakes_count = 0;
+uint8_t mistakes_count = 0;  /**< For games that count mistakes, should be checked for on Update in GameXXXX */
 
 /**
  * When a task is finished, this should be set to true,
